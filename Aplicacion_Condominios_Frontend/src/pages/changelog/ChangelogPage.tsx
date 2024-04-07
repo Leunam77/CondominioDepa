f0fc6b01f4effe10c3c7c9fecec7863da11855a7
Leunam77 <juan.ma6531@gmail.com>
2024-04-07 07:39:12 -0400

    subiendo proyecto a remoto

---
+++ Aplicacion_Condominios_Frontend/src/pages/changelog/ChangelogPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type Props = {};
+
+const ChangelogPage = (props: Props) => {
+  return (
+    <div>Pagina de Mantenimiento</div>
+  );
+};
+
+export default ChangelogPage;

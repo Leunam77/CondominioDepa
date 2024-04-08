3f4399944a41f99e9e661afe8239bedac8394912
alfredLazaro <alfredolazaro999@gmail.com>
2024-04-08 00:04:17 -0400

    haciendo prueba nuevamente pero sin respuesta

--- Aplicacion_Condominios_Backend/routes/api.php
+++ Aplicacion_Condominios_Backend/routes/api.php
@@ -23,33 +23,33 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::controller(DepartamentoCotroller::class)->group(function(){
-    Route::get('/departamentos','index')->name('departamentos.index');
-    Route::post('/departamento','store')->name('departamento.store');
-    Route::get('/departamento/{id}','show')->name('departamento.show');
-    Route::put('/departamento/{id}','update')->name('departamento.update');
-    Route::put('/departamento/{id}','destroy')->name('departamento.destroy');
+    Route::get('/departamentos','index');
+    Route::post('/departamento','store');
+    Route::get('/departamento/{id}','show');
+    Route::put('/departamento/{id}','update');
+    Route::put('/departamento/{id}','destroy');
 });
 
 Route::controller(BloqueController::class)->group(function(){
-    Route::get('/bloques','index')->name('bloques.index');
-    Route::post('/bloque','store')->name('bloque.store');
-    Route::get('/bloque/{id}','show')->name('bloque.show');
-    Route::put('/bloque/{id}','update')->name('bloque.update');
-    Route::put('/bloque/{id}','destroy')->name('bloque.destroy');
+    Route::get('/bloques','index');
+    Route::post('/bloque','store');
+    Route::get('/bloque/{id}','show');
+    Route::put('/bloque/{id}','update');
+    Route::put('/bloque/{id}','destroy');
 });
 
 Route::controller(EdificioController::class)->group(function(){
-    Route::get('/edificios','index')->name('edificios.index');
-    Route::post('/edificio','store')->name('edificio.store');
-    Route::get('/edificio/{id}','show')->name('edificio.show');
-    Route::put('/edificio/{id}','update')->name('edificio.update');
-    Route::put('/edificio/{id}','destroy')->name('edificio.destroy');
+    Route::get('/edificios','index');
+    Route::post('/edificio','store');
+    Route::get('/edificio/{id}','show');
+    Route::put('/edificio/{id}','update');
+    Route::put('/edificio/{id}','destroy');
 });
 
 Route::controller(PisoController::class)->group(function(){
-    Route::get('/pisos','index')->name('pisos.index');
-    Route::post('/piso','store')->name('piso.store');
-    Route::get('/piso/{id}','show')->name('piso.show');
-    Route::put('/piso/{id}','update')->name('piso.update');
-    Route::put('/piso/{id}','destroy')->name('piso.destroy');
+    Route::get('/pisos','index');
+    Route::post('/piso','store');
+    Route::get('/piso/{id}','show');
+    Route::put('/piso/{id}','update');
+    Route::put('/piso/{id}','destroy');
 });

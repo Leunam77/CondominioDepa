0f097ca6e8bb7ea74d8207356984507ea033ac5f
alfredLazaro <alfredolazaro999@gmail.com>
2024-04-09 22:19:05 -0400

    puse una prueba en bloque seeder

--- Aplicacion_Condominios_Backend/database/seeders/BloqueSeeder.php
+++ Aplicacion_Condominios_Backend/database/seeders/BloqueSeeder.php
@@ -15,5 +15,16 @@ class BloqueSeeder extends Seeder
     {
         //
         Bloque::factory()->withoutTimestamps()->count(5)->create();
+
+        /* $bloqs=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+        foreach($bloqs as $bloq){
+            Bloque::create([
+                'nombre_bloque' => 'Bloque '.$bloq,
+                'direccion_bloque' => 'Direccion del Bloque '.$bloq,
+                'descripcion_bloque' => 'Descripcion del Bloque '.$bloq,
+                'imagen_bloque' => 'https://picsum.photos/640/480'
+            ]); 
+        }*/
+        
     }
 }

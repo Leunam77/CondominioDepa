8ad19d07553728fbdfcd2be1c647f90f944f716c
Erick <maumbeltran@gmail.com>
2024-04-11 22:05:41 -0400

    mantenimiento page

--- Aplicacion_Condominios_Frontend/src/pages/changelog/ChangelogPage.tsx
+++ Aplicacion_Condominios_Frontend/src/pages/changelog/ChangelogPage.tsx
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import "./style.css";
+import { CategoryForm } from "./categoryService/registerCategory/CategoryForm";
+import CategoryList from "./categoryService/categoryList/CategoryList";
+import { getAllCategories } from "../../mantenimiento/services/maintenance/categoryService";
 
 type Props = {};
 
 const ChangelogPage = (props: Props) => {
+  const [categories, setCategories] = useState<
+    { id: number; catnombre: string; catdescripcion: string }[]
+  >([]);
+
+  const [flag, setFlag] = useState(true);
+
+  const handleRegisterCategory = (
+    id: number,
+    catnombre: string,
+    catdescripcion: string
+  ) => {
+    setCategories([...categories, { id, catnombre, catdescripcion }]);
+  };
+
+  useEffect(() => {
+    getCategories();
+  }, [flag]);
+
+  const getCategories = async () => {
+    try {
+      const categoriesData = await getAllCategories();
+      setCategories(categoriesData);
+    } catch (error) {}
+  };
+
   return (
-    <div>Pagina de Mantenimiento</div>
+    <>
+      <h2>CATEGORÍAS DE SERVICIOS</h2>
+      <div id="content">
+        <CategoryForm onRegister={handleRegisterCategory} />
+        <CategoryList categories={categories} flag={flag} setFlag={setFlag} />
+      </div>
+    </>
   );
 };
 

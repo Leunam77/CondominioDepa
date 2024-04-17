ecdb8e4e1e2e5f1d11db45da0e2be7b47025a242
alfredLazaro <alfredolazaro999@gmail.com>
2024-04-17 02:46:38 -0400

    se genero modelo y migracion contrato

---
+++ Aplicacion_Condominios_Backend/app/Http/Controllers/Departamento/ContratoController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Departamento;
+
+use App\Http\Controllers\Controller;
+use App\Models\GestDepartamento\Contrato;
+use Illuminate\Http\Request;
+
+class ContratoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\GestDepartamento\Contrato  $contrato
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Contrato $contrato)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\GestDepartamento\Contrato  $contrato
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Contrato $contrato)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\GestDepartamento\Contrato  $contrato
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Contrato $contrato)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\GestDepartamento\Contrato  $contrato
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Contrato $contrato)
+    {
+        //
+    }
+}

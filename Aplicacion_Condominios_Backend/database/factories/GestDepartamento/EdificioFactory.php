57bf6eaa12680ea16e5c354f706ff78b2f12ab8f
alfredLazaro <alfredolazaro999@gmail.com>
2024-04-19 19:20:26 -0400

    corregi pequeñor errores y agregue generacion de imagenes a edificio

--- Aplicacion_Condominios_Backend/database/factories/GestDepartamento/EdificioFactory.php
+++ Aplicacion_Condominios_Backend/database/factories/GestDepartamento/EdificioFactory.php
@@ -14,11 +14,19 @@ class EdificioFactory extends Factory
     //protected $model = edificio::class;
     public function definition()
     {
+        $directory = public_path('departamento/images/edificios');
+        $ruta = 'departamento/images/edificios';
+        // Define el nuevo camino donde se almacenarán las imágenes
+        if (!\File::isDirectory($directory)) {
+            \File::makeDirectory($directory, 0755, true, true);
+        }
+        $imageName = $this->faker->image($directory, 500, 512, null, false);
+        $imagePath = $imageName ? $ruta . '/' . $imageName : 'departamento/images/edificios/edificio_default.jpg';
         return [
             //
             'nombre_edificio' => $this->faker->name,
             'descripcion_edificio' => $this->faker->text(200),
-            'imagen_edificio' => $this->faker->imageUrl($width = 640, $height = 480),
+            'imagen_edificio' => $imagePath,
             'cantidad_pisos' => $this->faker->numberBetween(1, 10),
             'bloque_id' => Bloque::factory()->create()->id
         ];

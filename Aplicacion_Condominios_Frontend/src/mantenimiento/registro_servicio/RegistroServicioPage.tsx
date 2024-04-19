3133aedd68dae02f03c748ddbadb493035efa4e1
and312 <andreapelaez436@gmail.com>
2024-04-18 20:46:35 -0400

    update style css

--- Aplicacion_Condominios_Frontend/src/mantenimiento/registro_servicio/RegistroServicioPage.tsx
+++ Aplicacion_Condominios_Frontend/src/mantenimiento/registro_servicio/RegistroServicioPage.tsx
@@ -194,7 +194,7 @@ export default function PersonalPage() {
                 onChange={handleChangeUbicacion}
               />
             </div>
-            <button
+            <button 
               className="block"
               type="submit"
               onClick={handleClickRegistrar}

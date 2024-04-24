4cf8801bcaad2d1cf9cb1481069a05fe75d07079
alfredLazaro <alfredolazaro999@gmail.com>
2024-04-24 19:18:49 -0400

    cree pero no implemente fechas fin sera mayor a fecha inicio

--- Aplicacion_Condominios_Backend/database/factories/GestDepartamento/ContratoFactory.php
+++ Aplicacion_Condominios_Backend/database/factories/GestDepartamento/ContratoFactory.php
@@ -13,12 +13,15 @@ class ContratoFactory extends Factory
      */
     public function definition()
     {
+        //si quiero que la fecha fin sea mayor a la fecha inicio
+        $fecha_inicio = $this->faker->dateTimeBetween('-1 year', 'now');
+        $fecha_fin = $this->faker->dateTimeBetween($fecha_inicio, '+1 year');
         return [
             //generar datos aleatorios
             'fecha_inicio_contrato' => $this->faker->date(),
             'fecha_fin_contrato' => $this->faker->date(),
             'precio_contrato' => $this->faker->randomFloat(2, 1000, 10000),
-            'tipo_contrato' => $this->faker->randomElement(['alquiler', 'venta']),
+            'tipo_contrato' => $this->faker->randomElement(['alquiler', 'anticretico']),
             'vigente_contrato' => true,
             'departamento_id' => Departamento::all()->random()->id,
             

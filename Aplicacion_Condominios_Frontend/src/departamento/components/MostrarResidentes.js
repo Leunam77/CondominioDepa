62778a43e103d2e0b9038aa14450bbc8fe6cd55c
Leunam77 <juan.ma6531@gmail.com>
2024-04-28 13:10:23 -0400

    correccion en visualizacion de residentes

--- Aplicacion_Condominios_Frontend/src/departamento/components/MostrarResidentes.js
+++ Aplicacion_Condominios_Frontend/src/departamento/components/MostrarResidentes.js
@@ -1,17 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import './DepartamentosCss.css';
+import './customs.css';
 
-import { Link } from "react-router-dom";
-import Cookies from 'universal-cookie';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card, CardImg, CardBody, CardTitle , Button } from 'reactstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare , faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { Card, CardImg, CardBody, CardTitle, Row, Col, Container, Label, CardText } from 'reactstrap';
+
 
 const endpoint = 'http://localhost:8000/api';
 const endpointImg = 'http://localhost:8000';
-const cookies = new Cookies();
+
 const MostrarResidentes = () => {
     const [residentes, setResidentes] = useState([]);
     const [contratos, setContratos] = useState({});
@@ -59,42 +57,45 @@ const MostrarResidentes = () => {
             console.error('Error al obtener los datos:', error);
         }
     };
-      
-    return(
-        <div className="Deps">
-            <h1 className="title">Residentes</h1>
-        
-            <div className= "lista">
-                {residentes.map((residente) => {
-                    const contrato = contratos[residente.contrato_id];
-                    const departamento = departamentos[contrato.departamento_id];
-                    const edificio = edificios[departamento.edificio_id];
-                    const bloque = bloques[edificio.bloque_id];
 
-                    return (
-                        <Card className="cardDepa" key={residente.id}>
-                            <CardImg
-                                alt="Card image cap"
-                                src={`${endpointImg}/${residente.imagen_residente}`}
-                                top
-                                width="100%"
-                            />
-                            <CardBody>
-                                <CardTitle tag="h5">{residente.nombre_residente} {residente.apellidos_residente}</CardTitle>
-                                <div className="botones">
-                                    <h3> DNI: {residente.cedula_residente}</h3>
-                                    <h3> Bloque: {bloque?.nombre_bloque || 'Ninguno'}</h3>
-                                    <h3> Edificio: {edificio?.nombre_edificio || 'Ninguno'}</h3>
-                                    <h3> Departamento: {departamento?.nombre_departamento || 'Ninguno'}</h3>
-                                    <h3> Celular: {residente.telefono_residente}</h3>
-                                    <h3> Tipo de residente: {residente.tipo_residente}</h3>
-                                </div>
-                            </CardBody>
-                        </Card>
-                    );
-                })}
-            </div>
-        </div>
+    return (
+        <>
+            <Container>
+                <Row >
+                    <Label className="text-center mb-4 titulosForms">Residentes</Label>
+                    {residentes.map((residente) => {
+                        const contrato = contratos[residente.contrato_id];
+                        const departamento = departamentos[contrato.departamento_id];
+                        const edificio = edificios[departamento.edificio_id];
+                        const bloque = bloques[edificio.bloque_id];
+
+                        return (
+                            <Col sm={2} key={residente.id}>
+                                <Card >
+                                    <CardImg
+                                        className="cardImgResidente"
+                                        alt="Card image cap"
+                                        src={`${endpointImg}/${residente.imagen_residente}`}
+                                        top
+                                    />
+                                    <CardBody>
+                                        <CardText>
+                                            <Label>{residente.nombre_residente} {residente.apellidos_residente}</Label>
+                                            <Label> DNI: {residente.cedula_residente}</Label>
+                                            <Label> Bloque: {bloque?.nombre_bloque || 'Ninguno'}</Label>
+                                            <Label> Edificio: {edificio?.nombre_edificio || 'Ninguno'}</Label>
+                                            <Label> Departamento: {departamento?.nombre_departamento || 'Ninguno'}</Label>
+                                            <Label> Celular: {residente.telefono_residente}</Label>
+                                            <Label> Tipo de residente: {residente.tipo_residente}</Label>
+                                        </CardText>
+                                    </CardBody>
+                                </Card>
+                            </Col>
+                        );
+                    })}
+                </Row>
+            </Container>
+        </>
     )
 }
 

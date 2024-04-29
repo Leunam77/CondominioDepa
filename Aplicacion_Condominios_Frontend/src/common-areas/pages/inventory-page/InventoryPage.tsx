f80aba02e0830e3c47a102fb2fdeb3cef56d2e60
AndrewHerrada <andrew.herrada.villarroel@gmail.com>
2024-04-29 01:07:33 -0400

    add table

--- Aplicacion_Condominios_Frontend/src/common-areas/pages/inventory-page/InventoryPage.tsx
+++ Aplicacion_Condominios_Frontend/src/common-areas/pages/inventory-page/InventoryPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ReactElement } from 'react'
+import React, { useState, ReactElement, useEffect } from 'react'
 
 interface ListElementProps {
     showForm: () => void;
@@ -30,13 +30,60 @@ const ListElement: React.FC<ListElementProps> = ({ showForm }) => {
     const [equipment, setEquipment] = useState([]);
 
     function fetchEquipment() {
+        fetch("http://localhost:3004/elements")
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Unexpected server response");
+                }
+                return response.json()
 
+            })
+            .then((data) => {
+                //console.log(data);
+                setEquipment(data);
+            })
+            .catch((error) => console.log("Error", error));
     }
+
+    useEffect(() => fetchEquipment(), []);
 
     return (
         <>
             <h2 className='text-center mb-3'> Lista de elementos </h2>
             <button onClick={showForm} type='button' className='btn btn-primary me-2'> Crear </button>
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>3</th>
+                        <th>4</th>
+                        <th>5</th>
+                        <th>6</th>
+                        <th>7</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        equipment.map((inventory, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{ }</td>
+                                    <td>{ }</td>
+                                    <td>{ }</td>
+                                    <td>{ }</td>
+                                    <td>{ }</td>
+                                    <td>{ }</td>
+                                    <td style={{ width: '10px', whiteSpace: 'nowrap' }}>
+                                        <button type='button' className='btn btn-primary btn-sm me-2'>Editar</button>
+                                        <button type='button' className='btn btn-danger btn-sm'>Eliminar</button>
+                                    </td>
+                                </tr>
+                            );
+                        })
+                    }
+                </tbody>
+            </table>
         </>
     );
 }

24ad50a4797c2e4ff90682e9f0cd4569f87bc827
alfredLazaro <alfredolazaro999@gmail.com>
2024-05-01 13:04:47 -0400

    correccion de mayusculas y minusculas

--- Aplicacion_Condominios_Backend/database/factories/GestDepartamento/ContratoFactory.php
+++ Aplicacion_Condominios_Backend/database/factories/GestDepartamento/ContratoFactory.php
@@ -21,7 +21,7 @@ class ContratoFactory extends Factory
             'fecha_inicio_contrato' => $fechaInicio,
             'fecha_fin_contrato' => $fechaFin,
             'precio_contrato' => $this->faker->randomFloat(2, 1000, 10000),
-            'tipo_contrato' => $this->faker->randomElement(['alquiler', 'anticretico','venta']),
+            'tipo_contrato' => $this->faker->randomElement(['Alquiler', 'Anticretico','Venta']),
             'vigente_contrato' => true,
             'departamento_id' => Departamento::all()->random()->id,
             

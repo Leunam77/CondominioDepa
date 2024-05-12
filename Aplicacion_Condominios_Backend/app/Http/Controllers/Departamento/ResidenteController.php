6e42a8da58ee4d238ea934e455c3d8ab948bb1e0
alfredLazaro <alfredolazaro999@gmail.com>
2024-05-12 17:22:46 -0400

    ya no es necesario league/csv

--- Aplicacion_Condominios_Backend/app/Http/Controllers/Departamento/ResidenteController.php
+++ Aplicacion_Condominios_Backend/app/Http/Controllers/Departamento/ResidenteController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Departamento;
 use App\Http\Controllers\Controller;
 use App\Models\GestDepartamento\Residente;
 use Illuminate\Http\Request;
-use League\Csv\Reader;
+
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Log;
 
@@ -227,7 +227,7 @@ class ResidenteController extends Controller
         return response()->json(['mensaje' => 'Atributo actualizado correctamente']);
     }
 
-    public function import(Request $request){
+    /* public function import(Request $request){
         $file = $request->file('file');
         //leer el archivo csv
         $csv = Reader::createFromPath($file->getPathname(), 'r');
@@ -263,10 +263,10 @@ class ResidenteController extends Controller
                 continue; //continuar con el siguiente registro
             }
             //crear un nuevo residente
-            /* $residente = new Residente();
-            $residente->nombre_residente = $record['nombre_residente'];
-            $residente->apellido_residente = $record['apellido_residente'];
-            $residente->cedula_residente = $record['cedula_residente']; */
+            //$residente = new Residente();
+            //$residente->nombre_residente = $record['nombre_residente'];
+            //$residente->apellido_residente = $record['apellido_residente'];
+            //$residente->cedula_residente = $record['cedula_residente']; 
             //si la validación es exitosa, crear un nuevo residente
             $residente = Residente::create([
                 'nombre_residente' => $record['nombre_residente'],
@@ -289,7 +289,7 @@ class ResidenteController extends Controller
             'status' => 200,
             'message' => 'Residentes importados exitosamente'
         ]);
-    }
+    } */
     
     public function getResidentesByContrato($id)
     {

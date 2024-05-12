e3e51dd7b0beb1f4928112b4997faadbd856294b
alfredLazaro <alfredolazaro999@gmail.com>
2024-05-12 13:19:32 -0400

    cree el modelo, migracion, controlador, factory. Desarrolle el modelo y la migracion

---
+++ Aplicacion_Condominios_Backend/app/Http/Controllers/Departamento/ParqueoController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Departamento;
+
+use App\Http\Controllers\Controller;
+use App\Models\GestDepartamento\Parqueo;
+use Illuminate\Http\Request;
+
+class ParqueoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\GestDepartamento\Parqueo  $parqueo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Parqueo $parqueo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\GestDepartamento\Parqueo  $parqueo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Parqueo $parqueo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\GestDepartamento\Parqueo  $parqueo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Parqueo $parqueo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\GestDepartamento\Parqueo  $parqueo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Parqueo $parqueo)
+    {
+        //
+    }
+}

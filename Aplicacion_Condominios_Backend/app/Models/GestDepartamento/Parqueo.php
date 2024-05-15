07ccb0281dc3fbc7269425413f2a30ca6bb390f2
alfredLazaro <alfredolazaro999@gmail.com>
2024-05-15 13:44:46 -0400

    ya se crean parqueos con fecha de creacion y actualizacion

--- Aplicacion_Condominios_Backend/app/Models/GestDepartamento/Parqueo.php
+++ Aplicacion_Condominios_Backend/app/Models/GestDepartamento/Parqueo.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Parqueo extends Model
 {
     use HasFactory;
-    public $timestamps = false;
+    //public $timestamps = false;
     protected $fillable = [
         'nombre_parqueo',
         'departamento_id'

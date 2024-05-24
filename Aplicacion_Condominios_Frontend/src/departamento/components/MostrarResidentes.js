d46a17ce80a351162d556bce9f5fe8c62c92b0eb
Leunam77 <juan.ma6531@gmail.com>
2024-05-24 13:18:55 -0400

    se optimizo las consultas a la api, se obtiene bloques y depas en visparq, regEdi corregido

--- Aplicacion_Condominios_Frontend/src/departamento/components/MostrarResidentes.js
+++ Aplicacion_Condominios_Frontend/src/departamento/components/MostrarResidentes.js
@@ -24,35 +24,44 @@ const MostrarResidentes = () => {
 
     const getAllData = async () => {
         try {
-            const responseContratos = await axios.get(`${endpoint}/contratosVigentes`);
+            const [
+                responseContratos,
+                responseDepartamentos,
+                responseEdificios,
+                responseBloques,
+                responseResidentes
+            ] = await Promise.all([
+                axios.get(`${endpoint}/contratosVigentes`),
+                axios.get(`${endpoint}/departamentos`),
+                axios.get(`${endpoint}/edificios`),
+                axios.get(`${endpoint}/bloques`),
+                axios.get(`${endpoint}/residentes`)
+            ]);
+
             const contratosData = responseContratos.data.contratos.reduce((acc, contrato) => {
                 acc[contrato.id] = contrato;
                 return acc;
             }, {});
             setContratos(contratosData);
 
-            const responseDepartamentos = await axios.get(`${endpoint}/departamentos`);
             const departamentosData = responseDepartamentos.data.reduce((acc, departamento) => {
                 acc[departamento.id] = departamento;
                 return acc;
             }, {});
             setDepartamentos(departamentosData);
 
-            const responseEdificios = await axios.get(`${endpoint}/edificios`);
             const edificiosData = responseEdificios.data.reduce((acc, edificio) => {
                 acc[edificio.id] = edificio;
                 return acc;
             }, {});
             setEdificios(edificiosData);
 
-            const responseBloques = await axios.get(`${endpoint}/bloques`);
             const bloquesData = responseBloques.data.reduce((acc, bloque) => {
                 acc[bloque.id] = bloque;
                 return acc;
             }, {});
             setBloques(bloquesData);
 
-            const responseResidentes = await axios.get(`${endpoint}/residentes`);
             setResidentes(responseResidentes.data);
         } catch (error) {
             console.error('Error al obtener los datos:', error);

0d265b78cf55fa7515ab9eb3403f59a42e69f019
alfredLazaro <alfredolazaro999@gmail.com>
2024-06-01 23:21:42 -0400

    disminui la cantidad de bloques y edificios para los seeders

--- Aplicacion_Condominios_Backend/database/seeders/BloqueSeeder.php
+++ Aplicacion_Condominios_Backend/database/seeders/BloqueSeeder.php
@@ -14,7 +14,7 @@ class BloqueSeeder extends Seeder
     public function run()
     {
         //
-        Bloque::factory()->withoutTimestamps()->count(5)->create();
+        Bloque::factory()->withoutTimestamps()->count(3)->create();
 
         /* $bloqs=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
         foreach($bloqs as $bloq){

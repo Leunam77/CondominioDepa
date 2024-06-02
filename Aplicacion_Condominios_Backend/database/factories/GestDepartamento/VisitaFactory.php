b0692299d8a7210b9b47356a3b9d1df26e1b54bd
alfredLazaro <alfredolazaro999@gmail.com>
2024-06-01 23:37:35 -0400

    datos del factory visita

---
+++ Aplicacion_Condominios_Backend/database/factories/GestDepartamento/VisitaFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories\app\Http\Models\GestDepartamento;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\GestDepartamento\departamento;
+class VisitaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'nombre_visita' => $this->faker->name,
+            'apellidos_visita' => $this->faker->lastName,
+            'cedula_visita' => $this->faker->unique()->randomNumber(8),
+            'telefono_visita' => $this->faker->phoneNumber,
+            'activo_visita' => $this->faker->boolean,
+            'departamento_id' => departamento::factory()->create()->id
+        ];
+    }
+}

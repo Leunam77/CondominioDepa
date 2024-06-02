beb2507cbdb449d61eff95e64477b4e2dec700bc
alfredLazaro <alfredolazaro999@gmail.com>
2024-06-01 21:30:40 -0400

    agregue imagenes por defecto para bloques en lugar de generarlos con faker

--- Aplicacion_Condominios_Backend/database/factories/GestDepartamento/BloqueFactory.php
+++ Aplicacion_Condominios_Backend/database/factories/GestDepartamento/BloqueFactory.php
@@ -14,6 +14,12 @@ class BloqueFactory extends Factory
     //protected $model = Bloque::class;
     public function definition()
     {
+        $directory = public_path('departamento/images/bloques');
+        $ruta = 'departamento/images/bloques';
+        if (!\File::isDirectory($directory)) {
+            \File::makeDirectory($directory, 0755, true, true);
+        }
+        $listaImagenes = ['departamento/images/bloques/bloque_pred.jpeg', 'departamento/images/bloques/bloque (1).jpg', 'departamento/images/bloques/bloque (2).jpg', 'departamento/images/bloques/bloque (3).jpg', 'departamento/images/bloques/bloque (4).jpg', 'departamento/images/bloques/bloque (5).jpg', 'departamento/images/bloques/bloque (6).jpg', 'departamento/images/bloques/bloque (7).jpg', 'departamento/images/bloques/bloque (8).jpg', 'bloque_defecto.jpg'];
         $nombres = ['Bloque A', 'Bloque B', 'Bloque C', 'Bloque D', 'Bloque E', 'Bloque F', 'Bloque G', 'Bloque H', 'Bloque I', 'Bloque J', 'Bloque K', 'Bloque L', 'Bloque M', 'Bloque N', 'Bloque O', 'Bloque P', 'Bloque Q', 'Bloque R', 'Bloque S', 'Bloque T', 'Bloque U', 'Bloque V', 'Bloque W', 'Bloque X', 'Bloque Y', 'Bloque Z'];
         return [
             //
